acf8a0ad849667cdec24e22c9174d3e34aed6ffa
mku777 <mkm.kuz777@gmail.com>
2023-06-17 19:46:20 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={"/test-work-tweets/"}>
+        <BrowserRouter basename="/test-work-tweets">
           <App />
         </BrowserRouter>
       </PersistGate>
