feea0bb3cbc5eea9b98cee180aa9293fa9e8508e
mku777 <mkm.kuz777@gmail.com>
2023-06-17 19:36:18 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter
-          basename="/test-work-tweets">
+          basename="/test-work-tweets/">
           <App />
         </BrowserRouter>
       </PersistGate>
